bc0b4927d3275995552359ea882197f22d9dde1c
Mike Ray <mike.ray@btinternet.com>
2017-04-17 23:30:48 +0100

    On branch master Changes to be committed:   (use "git reset HEAD <file>..." to unstage)
    	new file:   etc/issue
    	new file:   etc/skel/.emacs.d/abbrev_defs
    	new file:   etc/skel/.emacs.d/custom.el
    	new file:   etc/skel/.emacs.d/elpa/archive-contents
    	new file:   etc/skel/.emacs.d/elpa/builtin-packages
    	new file:   etc/skel/.emacs.d/elpa/package.el
    	new file:   etc/skel/.emacs.d/init.el
    	new file:   etc/skel/.emacs.d/lisp/abbrev-prepare.el
    	new file:   etc/skel/.emacs.d/lisp/abbrev-prepare.elc
    	new file:   etc/skel/.emacs.d/lisp/c-code-folding-prepare.el
    	new file:   etc/skel/.emacs.d/lisp/c-code-folding-prepare.elc
    	new file:   etc/skel/.emacs.d/lisp/elpa-prepare.el
    	new file:   etc/skel/.emacs.d/lisp/elpa-prepare.elc
    	new file:   etc/skel/.emacs.d/lisp/markdown-prepare.el
    	new file:   etc/skel/.emacs.d/lisp/org-prepare.el
    	new file:   etc/skel/.emacs.d/lisp/org-prepare.elc
    	new file:   etc/skel/.emacs.d/lisp/text-mode-prepare.el
    	new file:   etc/skel/.emacs.d/lisp/text-mode-prepare.elc
    	new file:   etc/skel/.emacs.d/places

---
+++ etc/skel/.emacs.d/custom.el
@@ -0,0 +1,29 @@
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(dectalk-default-speech-rate 250)
+ '(dtk-allcaps-beep nil t)
+ '(dtk-capitalize nil t)
+ '(dtk-split-caps nil t)
+ '(dtk-use-tones t)
+ '(emacspeak-audio-indentation t)
+ '(emacspeak-character-echo nil)
+ '(emacspeak-ispell-max-choices 10)
+ '(emacspeak-speak-tooltips t)
+ '(erc-netsplit-mode t)
+ '(espeak-default-speech-rate 275)
+ '(save-place t nil (saveplace))
+ '(save-place-limit nil)
+ '(sentence-end-double-space nil)
+ '(ses-initial-column-width 15)
+ '(voice-indent-settings (quote (harry 0 3 1 3 all))))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
