18788a84cf705498d7d65393a3e06db036ac2d33
Kyle <kyle@free2.ml>
2018-01-17 18:25:25 -0500

    Copy from the file repository to the correct system root.

--- build/build.sh
+++ build/build.sh
@@ -82,7 +82,7 @@ echo -e "\n/dev/mmcblk0p1  /boot   vfat    defaults        0       0" >> $workdi
 set_password "${workdir}/root" root "${rootpass}"
 
 # Copy all modified files into the new system
-	cp -R ../files/* "${newroot}"
+	cp -R ../files/* "${workdir}/root"
 
 # Add the regular user and set its password
 add_user "${workdir}/root" "${username}"
